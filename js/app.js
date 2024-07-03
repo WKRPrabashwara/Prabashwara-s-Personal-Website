94fe1e435cbc0d7419cf6212fed4df1cbd82ba26
Rivibibu Prabashwara <wkprabashwara@gmail.com>
2024-07-03 07:22:03 -0700

    added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
+/* --------------- Grab elements from DOM --------------- */
 const header = document.querySelector("header");
 
 const first_skill = document.querySelector(".skill:first-child");
+const sk_counters = document.querySelectorAll(".counter span");
+const progress_bars = document.querySelectorAll(".skills svg circle");
 
-/* --------------- Grab elements from DOM --------------- */
+window.addEventListener("scroll", () => {
+    if (!skillsPlayed) skillsCounter();
+});
+
 
 /* --------------- Sticky Navbar --------------- */
 
@@ -26,6 +32,48 @@ let sr = ScrollReveal({
 // sr.reveal(".showcase-image", { origin: "top", delay: 700 });
 
 /* --------------- Skills Progress Bar Animation --------------- */
+
+function hasReached(el) {
+    let topPosition = el.getBoundingClientRect().top;
+    
+    if (window.innerHeight >= topPosition + el.offsetHeight) return true;
+    return false;
+}
+
+function updateCount(num, maxNum) {
+    let currentNum = +num.innerText;
+
+    if (currentNum < maxNum) {
+        num.innerText = currentNum + 1;
+        setTimeout(() => {
+            updateCount(num, maxNum);
+        }, 12);
+    }
+}
+
+let skillsPlayed = false;
+
+function skillsCounter() {
+    if (!hasReached(first_skill)) return;
+
+    skillsPlayed = true;
+
+    sk_counters.forEach((counter, i) => {
+        let target = +counter.dataset.target;
+        let strokeValue = 427 - 427 * (target /100);
+
+        progress_bars[i].style.setProperty("--target", strokeValue);
+
+        setTimeout(() => {
+            updateCount(counter, target);
+        }, 400);
+    });
+
+    progress_bars.forEach(
+        (p) => (p.style.animation = "progress 2s ease-in-out forwards")
+    );
+}
+
 
 /* --------------- Services Counter Animation --------------- */
 

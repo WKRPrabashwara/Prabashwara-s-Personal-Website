ae2cd984241aa14b6b58d2eda9ab65bc6f90be35
WKR Prabashwara <wkprabashwara@gmail.com>
2024-07-04 01:14:09 -0700

    added

--- js/app.js
+++ js/app.js
@@ -123,4 +123,12 @@ activeLink();
 
 humburger.addEventListener("click", () => {
     document.body.classList.toggle("open");
+    document.body.classList.toggle("stopScrolling");
 });
+
+links.forEach((link) =>
+    link.addEventListener("click", () => {
+        document.body.classList.remove("open");
+        document.body.classList.remove("stopScrolling");
+    })
+);
